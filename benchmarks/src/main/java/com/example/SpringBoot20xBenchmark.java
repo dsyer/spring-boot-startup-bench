f375c5184468f8e4d59e41d0018bb99795bd2634
Dave Syer <dsyer@pivotal.io>
2017-05-26 16:48:57 +0100

    Update for latest versions

---
+++ benchmarks/src/main/java/com/example/SpringBoot20xBenchmark.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2016-2017 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example;
+
+import java.io.File;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.TearDown;
+import org.openjdk.jmh.annotations.Warmup;
+
+@Measurement(iterations = 5)
+@Warmup(iterations = 1)
+@Fork(value = 2, warmups = 0)
+@BenchmarkMode(Mode.AverageTime)
+public class SpringBoot20xBenchmark {
+
+	private static final String CLASSPATH = "BOOT-INF/classes" + File.pathSeparator
+			+ "BOOT-INF/lib/*";
+
+	@Benchmark
+	public void fatJar(BasicState state) throws Exception {
+		state.run();
+	}
+
+	@Benchmark
+	public void explodedJarLauncher(BootState state) throws Exception {
+		state.run();
+	}
+
+	@Benchmark
+	public void explodedJarMain(MainState state) throws Exception {
+		state.run();
+	}
+
+	public static void main(String[] args) throws Exception {
+		BootState state = new BootState();
+		state.run();
+	}
+
+	@State(Scope.Benchmark)
+	public static class BasicState extends ProcessLauncherState {
+		public BasicState() {
+			super(".", "-jar", jarFile("com.example:demo:jar:20x:0.0.1-SNAPSHOT"),
+					"--server.port=0");
+		}
+
+		@TearDown(Level.Iteration)
+		public void stop() throws Exception {
+			super.after();
+		}
+	}
+
+	@State(Scope.Benchmark)
+	public static class BootState extends ProcessLauncherState {
+		public BootState() {
+			super("target/demo", "-cp", ".",
+					"org.springframework.boot.loader.JarLauncher", "--server.port=0");
+			unpack("target/demo", jarFile("com.example:demo:jar:20x:0.0.1-SNAPSHOT"));
+		}
+
+		@TearDown(Level.Iteration)
+		public void stop() throws Exception {
+			super.after();
+		}
+	}
+
+	@State(Scope.Benchmark)
+	public static class MainState extends ProcessLauncherState {
+		public MainState() {
+			super("target/demo", "-cp", CLASSPATH, "com.example.DemoApplication",
+					"--server.port=0");
+			unpack("target/demo", jarFile("com.example:demo:jar:20x:0.0.1-SNAPSHOT"));
+		}
+
+		@TearDown(Level.Iteration)
+		public void stop() throws Exception {
+			super.after();
+		}
+	}
+
+}

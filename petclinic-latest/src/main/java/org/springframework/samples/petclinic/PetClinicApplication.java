6352fd783a5880f19b2bfae195e2f3e6d4c1a477
Dave Syer <dsyer@pivotal.io>
2018-04-26 12:34:09 +0100

    Tidy up petclinic-latest

--- petclinic-latest/src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ petclinic-latest/src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -26,6 +26,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
+// @Import(LazyInitBeanFactoryPostProcessor.class)
 public class PetClinicApplication {
 
     public static void main(String[] args) throws Exception {
